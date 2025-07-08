0d6953db366f340c8ab199bb7ae8a7b40f654a47
cybrixtechnologies <cybrixtechnologies17@gmail.com>
2025-07-08 11:11:03 +0530

    Updated for tenants

--- public/js/main.js
+++ public/js/main.js
@@ -124,3 +124,15 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+// === Tawk.to Live Chat Integration ===
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script");
+  s1.src = "https://embed.tawk.to/686caab16bad05190e60cf00/1ivk7lhfh";
+  s1.async = true;
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  var s0 = document.getElementsByTagName("script")[0];
+  s0.parentNode.insertBefore(s1, s0);
+})();
+
